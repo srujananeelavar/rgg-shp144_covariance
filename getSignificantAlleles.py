5238ed40b0c14f67b9db4102144d8b1e035ba447
Srujana Neelavar <99630763+srujananeelavar@users.noreply.github.com>
2024-01-27 11:38:20 -0500

    Get alleles in more than 1% of db

--- getSignificantAlleles.py
+++ getSignificantAlleles.py
@@ -1,19 +1,3 @@
-# import pandas as pd
-
-# # Read the input Excel sheet
-# df = pd.read_excel('/Users/srujananeelavar/Documents/Summer2023/Research/Rgg144RiosSHP/SHP144/SHP144NT/SHP144AlleleCounts.xlsx')
-
-# # Calculate the total count of Sequence
-# total_count = df['Counts'].sum()
-
-# # Filter out rows with counts less than 5% of the total
-# df = df[df['Counts'] >= 0.01 * total_count]
-
-# # Rename the columns to Alleles and Frequency
-# df = df.rename(columns={'Alleles': 'Alleles', 'Counts': 'Counts'})
-
-# # Write the filtered results to a new Excel sheet
-# df.to_excel('/Users/srujananeelavar/Documents/Summer2023/Research/Rgg144RiosSHP/SHP144/SHP144NT/SHP144Sig1PercentAlleleCounts.xlsx', index=False)
 import pandas as pd
 
 # Prompt the user to input the file path for the input Excel sheet
